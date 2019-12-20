a70aebd824800f4bc2412ec967803508ce2413fd
Mircea Vlasceanu <mirceavlasceanu@gmail.com>
2019-12-20 23:29:43 +0200

    Adaugarea posibilitatii de a alege daca se copiaza fisierele sau se deschide o lista cu rezultate

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,13 +1,36 @@
 import os
 import win32api
-import PyInquirer 
+import PyInquirer
+import shutil
+import datetime
 
+#aduce data curenta
+now = datetime.datetime.now()
+#nume fisier temporar
+filepath = 'temp.txt'
+
+#aducem driverele din calculator
 def get_drives():
     drives = []
     string_partitii = win32api.GetLogicalDriveStrings()
     drives = list(string_partitii.split("\\\x00"))
     return drives
 
+def create_folder(path):
+    os.mkdir(path)
+
+def copiere_fisiere():
+    with open(filepath,'r') as fp:
+        lines = fp.read().splitlines()
+        for line in lines:
+            newPath = shutil.copy(str(line), d_path)
+    file.close()
+
+def curata_fisier(file):
+    open(file, 'w').close()
+
+
+#partea de interactiune cu utilizatorul
 questions =[{
     'type' : 'list',
     'name': 'drive',
@@ -18,23 +41,40 @@ questions =[{
     'type': 'input',
     'name': 'extensie_fisier',
     'message': 'Ce tip de fisier cauti ?',   
+},
+{
+    'type': 'list',
+    'name': 'copiere',
+    'message': 'Vrei sa copiezi fisierele ?',
+    'choices' : ['DA', 'NU'],
 }]
-
 answers = PyInquirer.prompt(questions)
 print(answers)
 
-
-#print (get_drives())
+#creaza fisierul cu raspunsuri
 path = answers['drive']
+#calea fisierelor daca se doreste copierea
+d_path = ("H:\\TESTE FISIERE\\" 
+         + answers['extensie_fisier'] 
+         + now.strftime("%Y-%m-%d %H_%M"))
 
+curata_fisier(filepath)
 try:
-    file = open("fisiere.txt","a")
+    file = open("temp.txt","a")
 finally:
     try:
         for (dirpath, dirnames, filenames ) in os.walk(path):
             for filename in filenames:
                 if filename.endswith("."+answers['extensie_fisier']):
-                    #file.write(entry + "\n")
-                    print(os.path.join(dirpath, filename))
+                    file.write(os.path.join(dirpath, filename) + "\n")
     finally:
         file.close()
+
+print ("fisierul rezultat a fost creat")
+
+
+if answers['copiere'] == 'DA':
+    create_folder(d_path)
+    copiere_fisiere()
+else:
+    os.startfile(filepath)
